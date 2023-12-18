b8b692322cbf13138961f7174e9eb037a4fc8121
RichardOhata <richardohata@gmail.com>
2023-12-17 19:11:54 -0800

    Added jump funcitonality and imported Pro Builder asset

--- Assets/Scripts/Game/PlayerMovement.cs
+++ Assets/Scripts/Game/PlayerMovement.cs
@@ -9,11 +9,22 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12f;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
 
+    Vector3 velocity;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    bool isGrounded;
     PhotonView view;
+
+
 
     void Start()
     {
+     
         view = GetComponent<PhotonView>();
         if (!view.IsMine)
         {
@@ -26,15 +37,31 @@ public class PlayerMovement : MonoBehaviour
     {
         if (view.IsMine)
         {
-            if (!GameObject.FindWithTag("PauseMenu").GetComponent<PauseMenuLogic>().isActive) { 
-            float x = Input.GetAxis("Horizontal");
-            float z = Input.GetAxis("Vertical");
+            if (!GameObject.FindWithTag("PauseMenu").GetComponent<PauseMenuLogic>().isActive) {
 
-            Vector3 move = transform.right * x + transform.forward * z;
+                isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
 
-            controller.Move(move * speed * Time.deltaTime);
+                if (isGrounded && velocity.y < 0)
+                {
+                    velocity.y = -2f;
+                }
+
+                float x = Input.GetAxis("Horizontal");
+                float z = Input.GetAxis("Vertical");
+
+                Vector3 move = transform.right * x + transform.forward * z;
+
+                controller.Move(move * speed * Time.deltaTime);
+
+                if(Input.GetButtonDown("Jump") && isGrounded)
+                {
+                    velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+                }
+
+                velocity.y += gravity * Time.deltaTime;
+
+                controller.Move(velocity * Time.deltaTime);
+            }
         }
-        }
-
     }
 }
