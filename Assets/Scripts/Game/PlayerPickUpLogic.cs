c48edb98930299d87c6150c21c1b23affa158630
RichardOhata <richardohata@gmail.com>
2023-12-18 18:01:42 -0800

    Added pickup and dropup functionality & reticle

--- Assets/Scripts/Game/PlayerPickUpLogic.cs
+++ Assets/Scripts/Game/PlayerPickUpLogic.cs
@@ -20,10 +20,17 @@ public class PlayerPickUpLogic : MonoBehaviour
     [Min(1)]
     private float hitRange = 3;
 
+    [SerializeField]
+    private Transform pickUpParent;
+
+    [SerializeField]
+    private GameObject inHandItem;
+
     private RaycastHit hit;
 
     private void Update()
     {
+
         Debug.DrawRay(playerCameraTransform.position, playerCameraTransform.forward * hitRange, Color.red);
         if (hit.collider != null)
         {
@@ -31,10 +38,47 @@ public class PlayerPickUpLogic : MonoBehaviour
             pickUpUI.SetActive(false);
 
         }
+
+       
+        if (inHandItem != null)
+        {
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+
+                if (hit.collider != null)
+                {
+                    inHandItem.transform.SetParent(null);
+                    inHandItem = null;
+                    Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                    if (rb != null)
+                    {
+                        rb.isKinematic = false;
+                    }
+                }
+
+
+            }
+            return;
+        }
+
         if (Physics.Raycast(playerCameraTransform.position, playerCameraTransform.forward, out hit, hitRange, pickableLayerMask))
         {
             hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
             pickUpUI.SetActive(true);
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                Rigidbody rb = hit.collider.GetComponent<Rigidbody>();
+                Debug.Log("Pressed E");
+                inHandItem = hit.collider.gameObject;
+                inHandItem.transform.position = Vector3.zero;
+                inHandItem.transform.rotation = Quaternion.identity;
+                inHandItem.transform.SetParent(pickUpParent.transform, false);
+                if (rb != null)
+                {
+                    rb.isKinematic = true;
+                }
+                return;
+            }
         }
     }
 }
